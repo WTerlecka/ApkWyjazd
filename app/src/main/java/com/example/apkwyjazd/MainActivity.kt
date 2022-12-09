b4a78578108bba6fe6c56c521cb4a56727996a70
sztoslol <kubuszeski05@wp.pl>
2022-12-09 15:33:25 +0100

    commit 3

--- app/src/main/java/com/example/apkwyjazd/MainActivity.kt
+++ app/src/main/java/com/example/apkwyjazd/MainActivity.kt
@@ -2,14 +2,13 @@ package com.example.apkwyjazd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.CalendarView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var lista = arrayListOf<TextView>(findViewById(R.id.pocz),findViewById(R.id.koniec))
+        var lista = arrayListOf<TextView>(findViewById(R.id.pocz1),findViewById(R.id.koniec1))
 
     }
 }
