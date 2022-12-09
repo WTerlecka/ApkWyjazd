6aa0275da714f2a6fe7a9558e242dcdb64844926
sztoslol <kubuszeski05@wp.pl>
2022-12-09 15:01:33 +0100

    commit 1

--- app/src/main/java/com/example/apkwyjazd/MainActivity.kt
+++ app/src/main/java/com/example/apkwyjazd/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.apkwyjazd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var lista = arrayListOf<TextView>(findViewById(R.id.pocz),findViewById(R.id.koniec))
+
     }
 }
