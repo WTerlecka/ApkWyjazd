ba659c06e74f56d993d82b97a8b2503db577f91d
sztoslol <kubuszeski05@wp.pl>
2022-12-16 15:39:21 +0100

    commit 8

--- app/src/main/java/com/example/apkwyjazd/MainActivity.kt
+++ app/src/main/java/com/example/apkwyjazd/MainActivity.kt
@@ -1,24 +1,26 @@
 package com.example.apkwyjazd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.0)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var kalendarz = findViewById<CalendarView>(R.id.calendarView)
+        var pocz = findViewById<Button>(R.id.btnstart)
+        var kon = findViewById<Button>(R.id.btnend)
+        var pocz2 = findViewById<TextView>(R.id.pocz)
+        var kon2 = findViewById<TextView>(R.id.koniec)
+        var il = findViewById<TextView>(R.id.il)
 
-        findViewById<Button>(R.id.btnstart).setOnClickListener {
-            var kalendarz = findViewById<CalendarView>(R.id.calendarView)
-            findViewById<TextView>(R.id.pocz).text = "Początek wyjazdu: " + kalendarz.date
-        }
 
-        findViewById<Button>(R.id.btnend).setOnClickListener {
-            var kalendarz = findViewById<CalendarView>(R.id.calendarView)
-            findViewById<TextView>(R.id.koniec).text = "Koniec wyjazdu: " + kalendarz.date
-        }
+
     }
 }
