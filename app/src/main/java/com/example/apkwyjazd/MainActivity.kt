fabb0ebca20c90fe8ef3338103e5b51f5aae5dc3
sztoslol <kubuszeski05@wp.pl>
2022-12-16 14:56:28 +0100

    commit 8

--- app/src/main/java/com/example/apkwyjazd/MainActivity.kt
+++ app/src/main/java/com/example/apkwyjazd/MainActivity.kt
@@ -10,13 +10,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val lista = arrayListOf<TextView>(findViewById(R.id.pocz2),findViewById(R.id.koniec2))
 
-        findViewById<Button>(R.id.Btndod).setOnClickListener {
-            val kalendarz = findViewById<CalendarView>(R.id.calendarView)
-            for(i in 0..2){
-                lista[i].text = kalendarz.date.toString()
-            }
+        findViewById<Button>(R.id.btnstart).setOnClickListener {
+            var kalendarz = findViewById<CalendarView>(R.id.calendarView)
+            findViewById<TextView>(R.id.pocz).text = "Początek wyjazdu: " + kalendarz.date
+        }
+
+        findViewById<Button>(R.id.btnend).setOnClickListener {
+            var kalendarz = findViewById<CalendarView>(R.id.calendarView)
+            findViewById<TextView>(R.id.koniec).text = "Koniec wyjazdu: " + kalendarz.date
         }
     }
 }
